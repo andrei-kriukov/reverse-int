449fc6bd77858e1ce7c3147dccd33a3f0bb45696
andrei kriukov <kryukov.kryukov@gmail.com>
2022-07-01 16:59:30 +0300

    feat: add code to the index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let result = '';
+    let string = n + '';
+    for (let i = string.length - 1; i >= 0 ; i--) {
+      if (string[i] === '-') {
+        continue;
+      }
+      result = result + string[i];
+    }
+    return result;
 }
